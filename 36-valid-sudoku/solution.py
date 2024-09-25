6eb01aa8aa6a032a11f946e3fd0e7059aa98ff68
vamsivipparla <vipparla.vamsikrishna@gmail.com>
2024-09-24 23:20:03 -0400

    commit solution 36 valid sudoku

---
+++ 36-valid-sudoku/solution.py
@@ -0,0 +1,60 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
+
+'''
+alternate
+
+def isValidSudoku(self, board):
+    return (self.is_row_valid(board) and
+            self.is_col_valid(board) and
+            self.is_square_valid(board))
+
+def is_row_valid(self, board):
+    for row in board:
+        if not self.is_unit_valid(row):
+            return False
+    return True
+
+def is_col_valid(self, board):
+    for col in zip(*board):
+        if not self.is_unit_valid(col):
+            return False
+    return True
+    
+def is_square_valid(self, board):
+    for i in (0, 3, 6):
+        for j in (0, 3, 6):
+            square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+            if not self.is_unit_valid(square):
+                return False
+    return True
+    
+def is_unit_valid(self, unit):
+    unit = [i for i in unit if i != '.']
+    return len(set(unit)) == len(unit)
+
+'''
