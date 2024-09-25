99abef218bee75bfe3aaf491d739e309599426a9
vamsivipparla <vipparla.vamsikrishna@gmail.com>
2024-09-24 20:47:14 -0400

    commit solution 238

---
+++ 238-prod-array-exceptself/solution.py
@@ -0,0 +1,17 @@
+def productExceptSelf(nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = [1] * (len(nums))
+
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+        
+nums = [1,2,3,4]
+print(productExceptSelf(nums))
